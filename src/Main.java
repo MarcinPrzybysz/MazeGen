13b097935d82aa9a9cb16d9149c33a03cb55b393
Stilee <31044893+Stilee@users.noreply.github.com>
2018-02-16 22:19:45 +0100

    main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int width = 10;
+        int length = 10;
+
+        MazeGen maze = new MazeGen(width, length);
+
+    }
+}
